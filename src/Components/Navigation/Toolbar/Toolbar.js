635dc8bf7a96411f7c4973926306b2eed640f409
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-07-29 23:51:26 +0500

    WORKING ON IT

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Title from "../../Title/Title";
 import classes from "./Toolbar.module.css";
+import NavigationItems from "../NavigationItems/NavigationItems";
 const Toolbar = () => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
       <Title />
-      <nav>....</nav>
+      <nav>
+        <NavigationItems />
+      </nav>
     </header>
   );
 };
