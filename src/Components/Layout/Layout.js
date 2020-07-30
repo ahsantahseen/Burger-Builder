fbe023463901bbeeeb56799d5636590bb9c010aa
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-07-30 16:51:34 +0500

    Ver 1.1 added footer

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideToolBar from "../Navigation/SideToolBar/SideToolBar";
+import Footerbar from "../UI/footerbar/footerbar";
 
 export const Layout = (props) => {
   const [showSideToolbar, setshowSideToolbar] = useState(false);
@@ -18,6 +19,7 @@ export const Layout = (props) => {
         clicked={SideToolBarCloseHandler}
       ></SideToolBar>
       <main className={classes.Content}>{props.children}</main>
+      <Footerbar></Footerbar>
     </Auxiliary>
   );
 };
---
+++ src/Components/UI/footerbar/footerbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./footerbar.module.css";
+
+const Footerbar = () => {
+  return (
+    <div className={classes.footerbar}>
+      <p>Made by Ahsan Tahseen, Powered By React</p>
+    </div>
+  );
+};
+
+export default Footerbar;
