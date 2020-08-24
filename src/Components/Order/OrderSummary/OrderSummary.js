1f70e110c1359def07e0376b907b125e70ecda9a
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-24 18:34:08 +0500

    Making the Checkout Page

--- src/Components/Order/OrderSummary/OrderSummary.js
+++ src/Components/Order/OrderSummary/OrderSummary.js
@@ -7,7 +7,7 @@ const OrderSummary = (props) => {
   return (
     <div className={classes.OrderSummary}>
       <h1>Enjoy your delicious Burger</h1>
-      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+      <div style={{ width: "100%", height: "300px", margin: "auto" }}>
         <Burger ingredients={props.ingredients}></Burger>
       </div>
       <Button Btntype="Danger" clicked>
