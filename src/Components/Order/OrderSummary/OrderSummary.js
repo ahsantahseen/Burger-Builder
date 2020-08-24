a5c48f1ca5d6df5646cf63c2a54b8d42a984366f
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-24 18:25:58 +0500

    Making the Checkout Page

--- src/Components/Order/OrderSummary/OrderSummary.js
+++ src/Components/Order/OrderSummary/OrderSummary.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
 
 const OrderSummary = (props) => {
   return (
     <div>
       <h1>Enjoy your delicious Burger</h1>
+      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+        <Burger ingredients={props.ingredients}></Burger>
+      </div>
+      <Button>CANCEL</Button>
     </div>
   );
 };
