12b0ca152864f96ced2ff7e6a196cdf5b6c1640f
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-24 18:22:34 +0500

    Making the Checkout Page

---
+++ src/Components/Order/OrderSummary/OrderSummary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Burger from "../../Burger/Burger";
+
+const OrderSummary = (props) => {
+  return (
+    <div>
+      <h1>Enjoy your delicious Burger</h1>
+    </div>
+  );
+};
+
+export default OrderSummary;
