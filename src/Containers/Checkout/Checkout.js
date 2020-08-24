f15c52bae6a70f7e93c89f86a2a5bd32bb4d6812
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-24 18:33:24 +0500

    Making the Checkout Page

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import OrderSummary from "../../Components/Order/OrderSummary/OrderSummary";
 
 class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+    },
+  };
   render() {
     return (
       <div>
-        <OrderSummary></OrderSummary>
+        <OrderSummary ingredients={this.state.ingredients}></OrderSummary>
       </div>
     );
   }
