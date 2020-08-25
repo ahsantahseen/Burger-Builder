ac537a31869b544cf5d98d3ff7dcd84b53a61b3d
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-25 19:28:33 +0500

    Making the orders Page and Adjusting Navigation with NavLink

--- src/Components/Navigation/NavigationItems/NavigationItems.js
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,7 @@ const NavigationItems = () => {
       <NavigationItem link="/" active>
         Burger Builder
       </NavigationItem>
-      <NavigationItem link="/">Orders</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
   );
 };
