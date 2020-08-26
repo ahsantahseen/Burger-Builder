a2a21e9608fa5fc6415b8036da0bd3c4c67a41ad
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-27 00:15:36 +0500

    Working on Validation FeedBack

--- src/Components/UI/Forms/Input/Input.js
+++ src/Components/UI/Forms/Input/Input.js
@@ -3,12 +3,16 @@ import classes from "./Input.module.css";
 
 const Input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+  if (props.invalid) {
+    inputClasses.push(classes.Invalid);
+  }
   switch (props.elementtype) {
     case "input":
       inputElement = (
         <input
           {...props.elementconfig}
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           value={props.value}
           onChange={props.onChangeHandler}
         ></input>
@@ -18,7 +22,7 @@ const Input = (props) => {
       inputElement = (
         <textarea
           {...props.elementconfig}
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           value={props.value}
           onChange={props.onChangeHandler}
         ></textarea>
@@ -28,14 +32,17 @@ const Input = (props) => {
     case "select":
       inputElement = (
         <select
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           value={props.value}
           onChange={props.onChangeHandler}
         >
           {props.options.map((option) => {
             return (
-              <option key={option.value}
-          onChange={props.onChangeHandler} value={option.value}>
+              <option
+                key={option.value}
+                onChange={props.onChangeHandler}
+                value={option.value}
+              >
                 {option.displayValue}
               </option>
             );
@@ -47,7 +54,7 @@ const Input = (props) => {
       inputElement = (
         <input
           {...props.elementconfig}
-          className={classes.InputElement}
+          className={inputClasses.join(" ")}
           value={props.value}
           onChange={props.onChangeHandler}
         ></input>
--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -92,15 +92,15 @@ class ContactData extends Component {
       .catch((error) => this.setState({ loading: false, purchasing: false }));
   };
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
     return isValid;
